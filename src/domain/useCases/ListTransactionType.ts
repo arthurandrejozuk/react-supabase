24ea84c7efdf307909f7c8620d02295ba91bbae6
Arthur Avila <arthurpro295@gmail.com>
2025-03-13 11:52:36 -0300

    Criando Domains para Transações

---
+++ src/domain/useCases/ListTransactionType.ts
@@ -0,0 +1,8 @@
+import { ITransactionTypeRepository } from "../repositories/ITransactionTypeRepository";
+
+export class ListTransactionType {
+    constructor(private repository: ITransactionTypeRepository) {}
+    async execute() {
+        return this.repository.listAll();
+    }
+}
